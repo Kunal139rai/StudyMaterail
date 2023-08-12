a96e49f3f8746481f8a6e5ee4a017fe8c492d93e
Kunal139rai <kunal99rai@gmail.com>
2023-08-13 02:05:16 +0530

    First Commit

---
+++ Even_no.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+  int main (){
+     int i,n;
+
+
+     for(int i=0; i<=10; i++)
+           
+    cout<< n <<"*"<<i<< " = " << n*i;
+
+     
+
+     return 0;
+  }
---
+++ zero_sorted.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+  int main(){
+    int n ;
+     cin>> n;
+       
+
+        vector<int> v;
+
+        for(int i=0; i<=n; i++){
+            int ele; cin>>ele;
+            v.push_back(ele);
+
+        }
+
+        sortZeroesandOnes(v);
+
+       for( int i=0; i<n; i++) 
+
+  }
